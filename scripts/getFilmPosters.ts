3637e4eeb80dbf7d04b0e4ba434b21461874853d
Sam Bautista <tadeolinco@gmail.com>
2025-01-25 19:08:08 +0800

    Update posters

--- scripts/getFilmPosters.ts
+++ scripts/getFilmPosters.ts
@@ -7,16 +7,19 @@ import films from "../src/baseFilms.json" assert { type: "json" };
   const browser = await playwright.chromium.launch();
   const page = await browser.newPage();
 
-  for (const film of films) {
+  for (let i = 0; i < films.length; i++) {
+    const film = films[i];
     await page.goto(film.uri, { waitUntil: "domcontentloaded" });
     await page.waitForURL("**/film/**", { waitUntil: "domcontentloaded" });
     await page.waitForTimeout(3000);
+
+    console.log(`${film.name} (${i + 1} of ${films.length})`);
 
     const images = await page.$$eval(
       "#js-poster-col .film-poster.poster img",
       (all_images) => {
         const image_links = [];
-        all_images.forEach((image, index) => {
+        all_images.forEach((image) => {
           if (
             image.src.startsWith("https://") &&
             image.src.includes("resize")
@@ -28,8 +31,12 @@ import films from "../src/baseFilms.json" assert { type: "json" };
       }
     );
 
-    images.forEach((imageUrl, index) => {
-      const path = `src/posters/${film.name}.jpg`;
+    images.forEach((imageUrl) => {
+      const split = film.uri.split("/");
+      const path = `./src/posters/${split[split.length - 1]}/poster.jpg`;
+      fs.mkdirSync(`./src/posters/${split[split.length - 1]}`, {
+        recursive: true,
+      });
       const file = fs.createWriteStream(path);
       https.get(imageUrl, function (response) {
         response.pipe(file);
