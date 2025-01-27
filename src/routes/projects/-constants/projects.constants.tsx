04db1be0599ff4bb8c5790db5e2e0c4f43ebe417
Sam Bautista <tadeolinco@gmail.com>
2025-01-27 22:53:23 +0800

    Add projects

---
+++ src/routes/projects/-constants/projects.constants.tsx
@@ -0,0 +1,167 @@
+export type ProjectType = {
+  title: string;
+  startDate: Date;
+  endDate: Date | null;
+  description: string;
+  technologies: string[];
+  height: number;
+  noCover?: boolean;
+} & (
+  | {
+      image?: never;
+      video: string;
+    }
+  | {
+      image: string;
+      video?: never;
+    }
+);
+
+export const PROJECTS: ProjectType[] = [
+  {
+    title: "Portfolio Website",
+    startDate: new Date("2024-01-15"),
+    endDate: null,
+    description: "Modern portfolio website built with React and TypeScript",
+    technologies: ["React", "TypeScript", "Tailwind CSS", "Vite"],
+    height: 400,
+    video: "/videos/portfolio.mp4",
+  },
+
+  {
+    title: "Unravel Carbon - Dashboards",
+    startDate: new Date("2024-01-15"),
+    endDate: null,
+    description: "Dashboards built with React and TypeScript",
+    technologies: ["React", "TypeScript", "Tailwind CSS", "Vite"],
+    height: 800,
+    image: "/images/dashboards.jpeg",
+  },
+  {
+    title: "Unravel Carbon - Export",
+    startDate: new Date("2024-01-15"),
+    endDate: null,
+    description: "Export built with React and TypeScript",
+    technologies: ["React", "TypeScript", "Tailwind CSS", "Vite"],
+    height: 470,
+    image: "/images/chart-export.gif",
+  },
+  {
+    title: "Unravel Carbon - Compare",
+    startDate: new Date("2024-01-15"),
+    endDate: null,
+    description: "Export built with React and TypeScript",
+    technologies: ["React", "TypeScript", "Tailwind CSS", "Vite"],
+    height: 470,
+    image: "/images/compare.gif",
+  },
+  {
+    title: "Unravel Carbon - Climate Program",
+    startDate: new Date("2024-01-15"),
+    endDate: null,
+    description: "Dashboard built with React and TypeScript",
+    technologies: ["React", "TypeScript", "Tailwind CSS", "Vite"],
+    height: 380,
+    image: "/images/climate-program.jpeg",
+  },
+  {
+    title: "Unravel Carbon - Portfolio",
+    startDate: new Date("2024-01-15"),
+    endDate: null,
+    description: "Dashboard built with React and TypeScript",
+    technologies: ["React", "TypeScript", "Tailwind CSS", "Vite"],
+    height: 470,
+    image: "/images/portfolio.gif",
+  },
+  {
+    title: "Unravel Carbon - Data flow",
+    startDate: new Date("2024-01-15"),
+    endDate: null,
+    description: "Dashboard built with React and TypeScript",
+    technologies: ["React", "TypeScript", "Tailwind CSS", "Vite"],
+    height: 470,
+    image: "/images/data-flow.gif",
+  },
+  {
+    title: "Unravel Carbon - Data history",
+    startDate: new Date("2024-01-15"),
+    endDate: null,
+    description: "Dashboard built with React and TypeScript",
+    technologies: ["React", "TypeScript", "Tailwind CSS", "Vite"],
+    height: 470,
+    image: "/images/data-history.gif",
+  },
+  {
+    title: "Unravel Carbon - ISSB",
+    startDate: new Date("2024-01-15"),
+    endDate: null,
+    description: "Dashboard built with React and TypeScript",
+    technologies: ["React", "TypeScript", "Tailwind CSS", "Vite"],
+    height: 440,
+    image: "/images/ISSB.webp",
+  },
+  {
+    title: "Unravel Carbon - Assets",
+    startDate: new Date("2024-01-15"),
+    endDate: null,
+    description: "Dashboard built with React and TypeScript",
+    technologies: ["React", "TypeScript", "Tailwind CSS", "Vite"],
+    height: 470,
+    image: "/images/assets.gif",
+  },
+  {
+    title: "Unravel Carbon - Data export",
+    startDate: new Date("2024-01-15"),
+    endDate: null,
+    description: "Dashboard built with React and TypeScript",
+    technologies: ["React", "TypeScript", "Tailwind CSS", "Vite"],
+    height: 470,
+    image: "/images/data-export.gif",
+  },
+  {
+    title: "Unravel Carbon - Audit Trail",
+    startDate: new Date("2024-01-15"),
+    endDate: null,
+    description: "Dashboard built with React and TypeScript",
+    technologies: ["React", "TypeScript", "Tailwind CSS", "Vite"],
+    height: 470,
+    image: "/images/audit.gif",
+  },
+  {
+    title: "Unravel Carbon - Company Builder",
+    startDate: new Date("2024-01-15"),
+    endDate: null,
+    description: "Dashboard built with React and TypeScript",
+    technologies: ["React", "TypeScript", "Tailwind CSS", "Vite"],
+    height: 470,
+    image: "/images/company-builder.gif",
+  },
+  {
+    title: "Unravel Carbon - Quick Search",
+    startDate: new Date("2024-01-15"),
+    endDate: null,
+    description: "Dashboard built with React and TypeScript",
+    technologies: ["React", "TypeScript", "Tailwind CSS", "Vite"],
+    height: 470,
+    video: "/videos/quick-search.mp4",
+  },
+  {
+    title: "Insync",
+    startDate: new Date("2024-01-15"),
+    endDate: null,
+    description: "Dashboard built with React and TypeScript",
+    technologies: ["React", "TypeScript", "Tailwind CSS", "Vite"],
+    height: 575,
+    image: "/images/insync.gif",
+  },
+  {
+    title: "Gastos",
+    startDate: new Date("2024-01-15"),
+    endDate: null,
+    description: "Modern portfolio website built with React and TypeScript",
+    technologies: ["React", "TypeScript", "Tailwind CSS", "Vite"],
+    height: 800,
+    video: "/videos/gastos.mp4",
+    noCover: true,
+  },
+];
--- src/routes/projects/index.tsx
+++ src/routes/projects/index.tsx
@@ -1,22 +1,102 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
+import { Fragment, useEffect, useMemo, useState } from "react";
+import { PROJECTS, ProjectType } from "./-constants/projects.constants";
 
 export const Route = createFileRoute("/projects/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return (
-    <div className="min-h-dvh flex flex-col items-center justify-center bg-black">
-      <div className="flex flex-col gap-4 items-center justify-center">
-        <h1 className="text-white text-4xl font-bold">Projects</h1>
-        <img src="/meow_code.gif" />
-        <p className="text-white text-lg">WIP</p>
+  const [columnCount, setColumnCount] = useState(
+    window.innerWidth >= 992 ? 3 : window.innerWidth >= 768 ? 2 : 1
+  );
 
+  useEffect(() => {
+    const handleResize = () => {
+      setColumnCount(
+        window.innerWidth >= 992 ? 3 : window.innerWidth >= 768 ? 2 : 1
+      );
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const columnData = useMemo(() => {
+    const data: ProjectType[][] = [];
+    const columnHeights: number[] = [];
+
+    for (let i = 0; i < columnCount; i++) {
+      data.push([]);
+      columnHeights.push(0);
+    }
+
+    for (const project of PROJECTS) {
+      let shortestColumnIndex = 0;
+      let shortestColumnHeight = Infinity;
+      for (let i = 0; i < columnCount; i++) {
+        if (columnHeights[i] < shortestColumnHeight) {
+          shortestColumnHeight = columnHeights[i];
+          shortestColumnIndex = i;
+        }
+      }
+      columnHeights[shortestColumnIndex] += project.height;
+      data[shortestColumnIndex].push(project);
+    }
+
+    return data;
+  }, [columnCount]);
+
+  const renderColumn = (projects: ProjectType[]) => {
+    return (
+      <div className="flex flex-1 flex-col gap-4">
+        {projects.map((project) => (
+          <div
+            className="border border-neutral-700 flex flex-col bg-neutral-900 rounded-xl p-4 transition-all cursor-pointer duration-300 hover:scale-[101.25%]"
+            style={{
+              height: `${project.height / 35}%`,
+            }}
+          >
+            <p className="text-lg font-bold mb-4">{project.title}</p>
+            {project.video && (
+              <video
+                src={project.video}
+                playsInline
+                autoPlay
+                loop
+                muted
+                disableRemotePlayback
+                className={`w-full min-h-0 mx-auto flex-1 rounded-xl ${
+                  project.noCover ? "" : "object-cover"
+                }`}
+              />
+            )}
+            {project.image && (
+              <img
+                src={project.image}
+                className={`w-full min-h-0 mx-auto flex-1 rounded-xl ${
+                  project.noCover ? "" : "object-cover"
+                }`}
+              />
+            )}
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  return (
+    <div className="min-h-dvh p-4 bg-black">
+      <div className="mx-auto text-white">
         <Link to="/">
           <a className="text-white text-lg" role="button">
             {"<-"} go back
           </a>
         </Link>
+        <div className="flex mt-4 gap-4">
+          {columnData.map((projects, index) => (
+            <Fragment key={index}>{renderColumn(projects)}</Fragment>
+          ))}
+        </div>
       </div>
     </div>
   );
