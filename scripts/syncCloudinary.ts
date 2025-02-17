34773f106387d561af3faba22d6ee4a8c5067614
Sam Bautista <tadeolinco@gmail.com>
2025-02-17 14:55:21 +0800

    Update

--- scripts/syncCloudinary.ts
+++ scripts/syncCloudinary.ts
@@ -5,7 +5,7 @@ import fs from "fs";
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.VITE_CLOUDINARY_CLOUD,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
