10d3634d9045470b255e2b9aa3ed93a3d886dc5f
Sam Bautista <tadeolinco@gmail.com>
2025-02-23 11:44:53 +0800

    Remove plausible; add Vercel analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,15 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { Analytics } from "@vercel/analytics/react";
 
 export const Route = createRootRoute({
   component: RootComponent,
 });
 
 function RootComponent() {
-  return <Outlet />;
+  return (
+    <>
+      <Outlet />
+      <Analytics />
+    </>
+  );
 }
