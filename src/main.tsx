41cc732d339f0a780457c24eb04ae50118433a9a
Sam Bautista <tadeolinco@gmail.com>
2025-02-23 15:22:18 +0800

    Redirect to proper website

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,30 @@
-import {
-  createHashHistory,
-  createRouter,
-  RouterProvider,
-} from "@tanstack/react-router";
-import mixpanel from "mixpanel-browser";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "unfonts.css";
 import "./main.css";
 
-// Import the generated route tree
-import { routeTree } from "./routeTree.gen";
+// // Import the generated route tree
+// import { routeTree } from "./routeTree.gen";
 
-const memoryHistory = createHashHistory({});
+// const memoryHistory = createHashHistory({});
 
-// Create a new router instance
-const router = createRouter({ routeTree, history: memoryHistory });
+// // Create a new router instance
+// const router = createRouter({ routeTree, history: memoryHistory });
 
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
-}
+// declare module "@tanstack/react-router" {
+//   interface Register {
+//     router: typeof router;
+//   }
+// }
 
-// Near entry of your product, init Mixpanel
-mixpanel.init("a02956969c6328dac2035bc0fdb1065f", {
-  track_pageview: true,
-  persistence: "localStorage",
-});
+// // Near entry of your product, init Mixpanel
+// mixpanel.init("a02956969c6328dac2035bc0fdb1065f", {
+//   track_pageview: true,
+//   persistence: "localStorage",
+// });
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
-);
+// createRoot(document.getElementById("root")!).render(
+//   <StrictMode>
+//     <RouterProvider router={router} />
+//   </StrictMode>
+// );
+
+window.location.href = "https://tadeolinco.dev";
